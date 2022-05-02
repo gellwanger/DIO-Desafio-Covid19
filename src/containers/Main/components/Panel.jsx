959b57495362fad0139aebf534393c67ec70a4ef
gellwanger <gustavojce@gmail.com>
2022-05-01 21:54:59 -0300

    Ajusta alinhamento de itens

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -3,10 +3,12 @@ import { Card, Typography, Button, Select, MenuItem } from '../../../components'
 import COUNTRIES from '../../../commons/constants/countries'
 import { CardPanelContentStyled, ItemStyled } from './style'
 
+import '../../../commons/styles/PanelCard.css';
+
 const navigatorHasShare = navigator.share
 
 function Panel({ updateAt, onChange, data, country, getCoviddata }) {
-  const { cases, recovered, deaths, todayCases, todayDeaths } = data
+  const { recovered } = data
 
   const renderCountries = (country, index) => (
     <MenuItem key={`country-${index}`} value={country.value}>
@@ -50,9 +52,9 @@ function Panel({ updateAt, onChange, data, country, getCoviddata }) {
   return (
     <Card>
       <CardPanelContentStyled>
-        <div>
-          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
+        <div className="cardPanel">
+          <Typography variant="h5" component="span" color="primary">COVID19 </Typography>
+          <Typography variant="h6" component="span" color="primary">Painel Coronavírus </Typography>
           <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
           <div className="pt-2">
             <Select onChange={onChange} value={country}>
